b7c5ba6cd734fd5fdc4c7040d98ab6f95ce3e83f
miesch3u <nathanaelmiesch@gmail.com>
2023-12-11 09:36:41 +0100

    Création du MLP dans le main

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,6 +1,16 @@
+import MLP.MLP;
+import app.Parametrer;
+
 public class MainApp {
 
   public static void main(String[] args) {
-
+    Parametrer parametrer = new Parametrer();
+    if(args.length == 0) {
+      parametrer.parametrerDefaut();
+    }
+    else {
+      parametrer.parametrer();
+    }
+    MLP mlp = new MLP(parametrer.getNbNeurones(), parametrer.getTauxApprentissage(), parametrer.getFonctionActivation());
   }
 }
