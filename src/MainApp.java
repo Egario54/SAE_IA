84a49b9cf48548c52048194f62c3aefade2f2040
Egario-akier <thierry.dissidia@gmail.com>
2023-12-19 09:25:08 +0100

    [fix] Entraînement sur MNIST que 10 fois dûe à la condition, maintenant 1000 fois

--- src/MainApp.java
+++ src/MainApp.java
@@ -69,9 +69,10 @@ public class MainApp {
         }
       }
       //entraîner
-      if(i%100 == 99) System.out.println((i+1)+" : "+mlp.backPropagate(imagetteDouble, new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+      double result = mlp.backPropagate(imagetteDouble, new double[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
+      if(i%100 == 99) System.out.println((i+1) + " : " + result);
     }
 
-
   }
+
 }
